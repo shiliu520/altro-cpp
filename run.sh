86e82b27e71e4909802394ce6e34d0d04c65c159
shiliu <shiliu_fly@qq.com>
2025-11-16 11:47:45 +0800

    Generate compile_commands.json

--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
-# cd altro-cpp         # Change directory into directory root.
-mkdir build -p         # Create a build directory.
-cd build
-cmake ..               # Run the CMake configuration step. 
-cmake --build .        # Build all CMake targets
-make -j$(( $(nproc) / 2 ))
+# cd altro-cpp                         # Change directory into directory root.
+mkdir build -p                         # Create a build directory.
+cd build                               # Change directory into the build directory.
+cmake ..                               # Run the CMake configuration step.
+cmake -D ALTRO_BUILD_DOCS=ON ..        # Build all CMake targets
+cmake --build . --target altro_docs
+# make -j$(( $(nproc) / 2 ))
+make -j6
 # ctest .
